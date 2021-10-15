808c7c8ab049173e4a440d13755a5d5f701b4309
nishinoshungo <nishishun50@gmail.com>
2021-10-15 06:38:48 +0000

    マイグレーションの修正

--- db/migrate/20211014081524_add_columns_to_order_items.rb
+++ db/migrate/20211014081524_add_columns_to_order_items.rb
@@ -1,6 +1,6 @@
 class AddColumnsToOrderItems < ActiveRecord::Migration[5.2]
   def change
     add_column :order_items, :end_date, :datetime
-    add_column :order_items, :rental_status, :intager, default: 0
+    add_column :order_items, :rental_status, :integer, default: 0
   end
 end
